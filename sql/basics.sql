9a2ec24aa13c0a76c75200779b00376e4b73136e
Aliza Fatima <alizaf.work@gmail.com>
2026-01-07 08:55:02 -0500

    Create basics.sql
    Data Discovery (SELECT & FROM)

---
+++ sql/basics.sql
@@ -0,0 +1,38 @@
+## **Mastery of SELECT, FROM, WHERE, LIMIT, and LIKE**
+### Phase 1: Data Discovery (SELECT & FROM) <br>
+- The "Full Audit": How do you display every single row and column from the Streaming_Data table?<br>
+- The "Quick List": Write a query to show only the title, type, and genre for every entry in the table.<br>
+- The "Quick Preview": Show only the first 5 rows from the table to see how the content is formatted.<br>
+
+1. The "Full Audit": How do you display every single row and column from the Streaming_Data table?<br>
+  
+```
+  query = """
+        SELECT *
+        FROM Netflix;
+        """
+result = pd.read_sql(query, conn)
+result
+```
+2. The "Quick List": Write a query to show only the title, type, and genre for every entry in the table.<br>
+
+```
+query = """
+        SELECT title, type, genre
+        FROM Netflix;
+        """
+result = pd.read_sql(query, conn)
+result
+```
+3. The "Quick Preview": Show only the first 5 rows from the table to see how the content is formatted.
+
+  ```
+  query = """
+        SELECT *
+        FROM Netflix
+        LIMIT 5;
+        """
+result = pd.read_sql(query, conn)
+result.index = result.index + 1
+result
+```
