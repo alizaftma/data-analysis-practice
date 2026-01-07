5eeb8b4fe45dd1d64a50a67b7f4e60c00a2d2b26
Aliza Fatima <alizaf.work@gmail.com>
2026-01-07 09:34:53 -0500

    Create final.sql

---
+++ sql/final.sql
@@ -0,0 +1,22 @@
+-- Mastery of SELECT, FROM, WHERE, LIMIT, and LIKE
+-- Phase 5: The "Final Boss" Challenge
+
+-- 1. The Binge-Watch Report: Show the title and duration_min of the first 3 'TV Show' entries that are longer than 30 minutes.
+
+query = """
+        SELECT title, duration_min
+        FROM Netflix
+        WHERE type LIKE "TV SHOW" AND duration_min > 30;
+        """
+result = pd.read_sql(query, conn)
+result
+
+-- 2. The Specific Search: Find every title that starts with 'S', ends with 's', and belongs to the 'Sci-Fi' genre.
+
+query = """
+        SELECT title
+        FROM Netflix
+        WHERE title LIKE "S%" AND title LIKE "%s" AND genre = "Sci-Fi";
+        """
+result = pd.read_sql(query, conn)
+result
