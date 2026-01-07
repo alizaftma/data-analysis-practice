cedea80171c60bccf3f7e2e5c96babdb949da5e6
Aliza Fatima <alizaf.work@gmail.com>
2026-01-07 09:10:11 -0500

    Create basics.sql

--- sql/basics.sql
+++ sql/basics.sql
@@ -1,8 +1,5 @@
 -- Mastery of SELECT, FROM, WHERE, LIMIT, and LIKE
 -- Phase 1: Data Discovery (SELECT & FROM) 
--- The "Full Audit": How do you display every single row and column from the Streaming_Data table?<br>
--- The "Quick List": Write a query to show only the title, type, and genre for every entry in the table.<br>
--- The "Quick Preview": Show only the first 5 rows from the table to see how the content is formatted.<br>
 
 -- 1. The "Full Audit": How do you display every single row and column from the Streaming_Data table?<br>
   
