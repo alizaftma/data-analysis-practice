398167be0551e5e82753b07aa0874db8a5410981
Aliza Fatima <alizaf.work@gmail.com>
2026-01-07 09:01:38 -0500

    Create basics.sql
    Data Discovery (SELECT & FROM)

--- sql/basics.sql
+++ sql/basics.sql
@@ -1,32 +1,29 @@
-## **Mastery of SELECT, FROM, WHERE, LIMIT, and LIKE**
-### Phase 1: Data Discovery (SELECT & FROM) <br>
-- The "Full Audit": How do you display every single row and column from the Streaming_Data table?<br>
-- The "Quick List": Write a query to show only the title, type, and genre for every entry in the table.<br>
-- The "Quick Preview": Show only the first 5 rows from the table to see how the content is formatted.<br>
+-- Mastery of SELECT, FROM, WHERE, LIMIT, and LIKE
+-- Phase 1: Data Discovery (SELECT & FROM) 
+-- The "Full Audit": How do you display every single row and column from the Streaming_Data table?<br>
+-- The "Quick List": Write a query to show only the title, type, and genre for every entry in the table.<br>
+-- The "Quick Preview": Show only the first 5 rows from the table to see how the content is formatted.<br>
 
-1. The "Full Audit": How do you display every single row and column from the Streaming_Data table?<br>
+-- 1. The "Full Audit": How do you display every single row and column from the Streaming_Data table?<br>
   
-```
   query = """
         SELECT *
         FROM Netflix;
         """
 result = pd.read_sql(query, conn)
 result
-```
-2. The "Quick List": Write a query to show only the title, type, and genre for every entry in the table.<br>
 
-```
+-- 2. The "Quick List": Write a query to show only the title, type, and genre for every entry in the table.<br>
+
 query = """
         SELECT title, type, genre
         FROM Netflix;
         """
 result = pd.read_sql(query, conn)
 result
-```
-3. The "Quick Preview": Show only the first 5 rows from the table to see how the content is formatted.
 
-  ```
+-- 3. The "Quick Preview": Show only the first 5 rows from the table to see how the content is formatted.
+
   query = """
         SELECT *
         FROM Netflix
@@ -35,4 +32,4 @@ result
 result = pd.read_sql(query, conn)
 result.index = result.index + 1
 result
-```
+
